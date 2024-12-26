d836bdb15f201624fbe0e4f324a1bdaa8af54255
polaris0208 <sh6771@gmail.com>
2024-12-26 10:04:36 +0900

    [#5] feature : products 관리자 기능
    사용자를 위한 카테고리 생성 기능
    상품 게시물 관리 기능

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Products, Category
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "price", "quantity", "category"]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+admin.site.register(Products, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
